16ced14dc5947402fff3b015f91f28cd01bf9fa6
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-03-11 19:15:49 +0100

    lesson 9

---
+++ page-design.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+
+
+
+<div  class="container my-5">
+    <h1 class="text-center mb-5">Css Design Effects</h1>
+
+
+    <!-- Slider -->
+
+
+    <div class="slider">
+        <div class="slides">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/slide1.jpg">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/slide2.jpg">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/slide3.jpg">
+
+
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-md-4"></div>
+        <div class="rounded-box">
+            Rounded corners
+        </div>
+    </div>
+        <div class="row mt-5">
+        <div class="col-md-4"></div>
+        <div class="rounded-image-box">
+            border image example
+        </div>
+            <div class="row mt-4">
+
+        <div class="background-box">
+        </div>
+    </div>
+
+
+    </div>
+
+
+
+</div>
+
+
+
+
+
+<?php get_footer(); ?>
