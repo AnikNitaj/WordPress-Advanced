e497b34e4ad6d45bb6d8e9a8974f88ed7ac6aef0
AnikNitaj <aniknitaj1@gmail.com>
2026-03-25 19:06:01 +0100

    76

--- function.php
+++ function.php
@@ -34,4 +34,44 @@ function ds_theme_assets(){
     }
 }
 add_action('wp_enqueue_scripts', 'ds_theme_assets');
-?>
+
+
+function register_taxonomy_movie_genre() {
+    $labels = array(
+        'name'                  => __( 'Movie Genres', 'taxonomy general name' ),
+        'singular_name'         => __( 'Movie Genre', 'taxonomy singular name' ),
+        'search_items'          => __( 'Search Movie Genres' ),
+        'all_items'             => __( 'All Movie Genres' ),
+        'parent_item'           => __( 'Parent Movie Genre' ),
+        'parent_item_colon'     => __( 'Parent Movie Genre:' ),
+        'edit_item'             => __( 'Edit Movie Genre' ),
+        'update_item'           => __( 'Update Movie Genre' ),
+        'add_new_item'          => __( 'Add New Movie Genre' ),
+        'new_item_name'         => __('New Movie genre Name'),
+        'menu_name'             => __('Movie Genre'),
+    );
+
+    $args = array(
+        'hierarchical'          =>true,
+        'labels'                =>$labels,
+        'show_ui'               =>true,
+        'show_admin_column'     =>true,
+        'query_var'             =>true,
+        'show_in_rest'          =>true,
+        "rewrite"               =>array('slug' => 'movie-genre'),
+
+    );
+
+    register_taxonomy('movie_genres', array('movie'), $args);
+
+    register_taxonomy('movietags', 'movie', array(
+        'label'               => 'Movie Tags',
+        'rewrite'             => array('slug' => 'movie-tag'),
+        'hierarchical'        => false,
+        'show_ui'             => true,
+        'show_admin_column'   => true,
+        'show_in_rest'        => true,
+    ));
+
+}
+add_action( 'init', 'register_taxonomy_movie_genre' );
